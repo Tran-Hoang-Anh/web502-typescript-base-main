75a43d9847824a344c809becd822a6c393ea7605
TranHoangAnh <anhhoangtran021204@gmail.com>
2026-01-21 19:47:12 +0700

    your message

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,19 +1,82 @@
+import axios, { AxiosError } from "axios";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+
+type FormValues = {
+  name: string;
+  credit: number;
+};
+
+const validate = z.object({
+  name: z.string().min(3, "Name 3 ky tu").max(100),
+  credit: z.number().min(1).max(100),
+});
+
 function AddPage() {
+  const { id } = useParams(); // params: {id: '1'}
+  const nav = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(validate),
+  });
+
+  // get data theo id
+  useEffect(() => {
+    const getDetail = async () => {
+      try {
+        const { data } = await axios.get(`http://localhost:3000/courses/${id}`);
+        reset(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (id) {
+      getDetail();
+    }
+  }, [id]);
+
+  const onSubmit = async (values: FormValues) => {
+    try {
+      if (id) {
+        // edit
+        await axios.put(`http://localhost:3000/courses/${id}`, values);
+      } else {
+        // add
+        await axios.post("http://localhost:3000/courses", values);
+      }
+      toast.success("Thành công");
+      nav("/list");
+    } catch (error) {
+      console.log(error);
+      toast.error("Thất bại: " + (error as AxiosError).message);
+    }
+  };
+
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-6">Thêm mới</h1>
 
-      <form className="space-y-6">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
         {/* Text input */}
         <div>
           <label htmlFor="text" className="block font-medium mb-1">
             Text
           </label>
           <input
+            {...register("name")}
             type="text"
             id="text"
             className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
+          <span>{errors?.name?.message}</span>
         </div>
 
         {/* Select */}
--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,7 +1,97 @@
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+
+type FormValues = {
+  name: string;
+};
+
+const validate = z.object({
+  name: z.string().min(3, "Name 3 ky tu").max(10),
+  credit: z.number().min(1).max(100),
+});
+
 function EditPage() {
+  const { id } = useParams(); // params: {id: '1'}
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(validate),
+  });
+
+  // get data theo id
+  useEffect(() => {
+    const getDetail = async () => {
+      try {
+        const { data } = await axios.get(`http://localhost:3000/courses/${id}`);
+        reset(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (id) {
+      getDetail();
+    }
+  }, [id]);
+
+  const onSubmit = async (values: FormValues) => {
+    try {
+      // edit
+      await axios.put(`http://localhost:3000/courses/${id}`, values);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Cập nhật</h1>
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold mb-6">Update</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+        {/* Text input */}
+        <div>
+          <label htmlFor="text" className="block font-medium mb-1">
+            Text
+          </label>
+          <input
+            {...register("name")}
+            type="text"
+            id="text"
+            className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <span>{errors?.name?.message}</span>
+        </div>
+
+        {/* Select */}
+        <div>
+          <label htmlFor="selectOption" className="block font-medium mb-1">
+            Select - option
+          </label>
+          <select
+            id="selectOption"
+            className="w-full border rounded-lg px-3 py-2 bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+
+        {/* Submit button */}
+        <button
+          type="submit"
+          className="px-5 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
